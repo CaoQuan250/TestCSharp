2f1bacc113e04e39a765d54d969d13ae3cd08ee6
CaoQuan250 <ggxhffcdgd@gmail.com>
2022-08-31 10:28:39 +0700

    first commit

---
+++ CSharpTest/Ex2/Program.cs
@@ -0,0 +1,91 @@
+﻿namespace Ex2
+{
+    internal abstract class Animal
+    {
+        public abstract int Weight { get; set;}
+        public abstract string Name { get; set; }
+        public abstract void Show();
+        public abstract void SetMe(int weight, string name);
+        public Animal(int weight, string name)
+        {
+            Weight = weight;
+            Name = name;
+        }
+        public Animal() { }
+    }
+    internal class Tiger : Animal
+    {
+        public override int Weight { get; set; }
+        public override string Name { get; set; }
+        public Tiger() { }
+        public Tiger(int weight, string name) : base(weight, name)
+        {
+            Weight = weight;
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return "Name :" + Name + " Weight :" + Weight;
+        }
+        public override void SetMe(int weight, string name)
+        {
+           Weight = weight;
+            Name = name;
+        }
+
+        public override void Show()
+        {
+            Name = "Tiger";
+            Weight = 200;
+            Console.WriteLine(ToString());
+        }
+    }
+    internal class Lion : Animal
+    {
+        public override int Weight { get; set; }
+        public override string Name { get; set; }
+
+        public Lion() { }
+        public Lion(int weight, string name) : base(weight, name)
+        {
+            Weight = weight;
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return "Name :" + Name + " Weight :" + Weight;
+        }
+
+        public override void SetMe(int weight, string name)
+        {
+            
+            Weight = weight;
+            Name = name;
+        }
+
+        public override void Show()
+        {
+            Name = "Lion";
+            Weight = 100;
+            Console.WriteLine(ToString());
+        }
+
+
+    }
+    internal class Run
+    {
+        public static void Main(string[] cmd)
+        {
+            
+            Lion lion = new Lion();
+            lion.Show();
+            
+            Tiger tiger = new Tiger();
+            tiger.Show();
+            
+
+        }
+    }
+
+
+}
---
+++ CSharpTest/Test/Program.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bai2
+{
+    internal abstract class tbase
+    {
+        public abstract int Weight
+        {
+            get; set;
+        }
+        public abstract string Name { get; set; }
+        public abstract void Show();
+        public abstract void SetMe(int weight, string name);
+        public tbase(int weight, string name)
+        {
+            Weight = weight;
+            Name = name;
+        }
+        public tbase() { }
+    }
+    internal class Tiger : tbase
+    {
+        public override int Weight { get; set; }
+        public override string Name { get; set; }
+        public Tiger() { }
+        public Tiger(int weight, string name) : base(weight, name)
+        {
+            Weight = weight;
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return "Name :" + Name + " Weight :" + Weight;
+        }
+        public override void SetMe(int weight, string name)
+        {
+            Console.WriteLine("Fix Weight , Fix Name :");
+            Weight = weight;
+            Name = name;
+        }
+
+        public override void Show()
+        {
+            Name = "Tiger";
+            Weight = 200;
+            Console.WriteLine(ToString());
+        }
+    }
+    internal class Lion : tbase
+    {
+        public override int Weight { get; set; }
+        public override string Name { get; set; }
+
+        public Lion() { }
+        public Lion(int weight, string name) : base(weight, name)
+        {
+            Weight = weight;
+            Name = name;
+        }
+        public override string ToString()
+        {
+            return "Name :" + Name + " Weight :" + Weight;
+        }
+
+        public override void SetMe(int weight, string name)
+        {
+            Console.WriteLine("Fix Weight , Fix Name :");
+            Weight = weight;
+            Name = name;
+        }
+
+        public override void Show()
+        {
+            Name = "Lion";
+            Weight = 100;
+            Console.WriteLine(ToString());
+        }
+
+
+    }
+    internal class program
+    {
+        public static void Main(string[] cmd)
+        {
+            Console.WriteLine("------------");
+            Lion lion = new Lion();
+            lion.Show();
+            Console.WriteLine("------------");
+            Tiger tiger = new Tiger();
+            tiger.Show();
+            Console.WriteLine("------------");
+
+        }
+    }
+}
+
+
